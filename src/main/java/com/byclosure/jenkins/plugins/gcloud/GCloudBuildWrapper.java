fc771b561d0f22e912ec76016d1f31e21bfa68ff
Nicolas De Loof <nicolas.deloof@gmail.com>
2015-07-08 11:23:42 +0200

    GCloud SDK auto-installer

--- src/main/java/com/byclosure/jenkins/plugins/gcloud/GCloudBuildWrapper.java
+++ src/main/java/com/byclosure/jenkins/plugins/gcloud/GCloudBuildWrapper.java
@@ -1,15 +1,28 @@
 package com.byclosure.jenkins.plugins.gcloud;
 
+import com.cloudbees.jenkins.plugins.gcloudsdk.GCloudInstallation;
+import com.cloudbees.plugins.credentials.CredentialsMatcher;
+import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
+import com.cloudbees.plugins.credentials.domains.URIRequirementBuilder;
 import com.google.jenkins.plugins.credentials.domains.RequiresDomain;
+import com.google.jenkins.plugins.credentials.oauth.GoogleRobotPrivateKeyCredentials;
 import hudson.Extension;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.BuildListener;
+import hudson.model.Item;
+import hudson.security.ACL;
 import hudson.tasks.BuildWrapper;
 import hudson.tasks.BuildWrapperDescriptor;
+import hudson.tools.ToolInstallation;
+import hudson.util.ListBoxModel;
 import net.sf.json.JSONObject;
+import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 
 import java.io.IOException;
@@ -19,15 +32,21 @@ import java.util.logging.Logger;
 @RequiresDomain(value = GCloudScopeRequirement.class)
 public class GCloudBuildWrapper extends BuildWrapper {
 	private static final Logger LOGGER = Logger.getLogger(GCloudBuildWrapper.class.getName());
+
+	private final String installation;
 	private final String credentialsId;
 
 	@DataBoundConstructor
-	public GCloudBuildWrapper(String credentialsId) {
-		this.credentialsId = credentialsId;
-	}
+	public GCloudBuildWrapper(String installation, String credentialsId) {
+        this.installation = installation;
+        this.credentialsId = credentialsId;
+    }
 
 	@Override
 	public Environment setUp(AbstractBuild build, Launcher launcher, BuildListener listener) throws IOException, InterruptedException {
+
+        final ToolInstallation sdk = getSDK().translate(build, listener);
+
 		final GCloudServiceAccount serviceAccount =
 				GCloudServiceAccount.getServiceAccount(build, launcher, listener, credentialsId);
 
@@ -55,7 +74,22 @@ public class GCloudBuildWrapper extends BuildWrapper {
 		};
 	}
 
-	public String getCredentialsId() {
+    public GCloudInstallation getSDK() {
+        GCloudInstallation[] installations = GCloudInstallation.getInstallations();
+        if (installations.length == 1) return installations[0];
+        for (GCloudInstallation sdk : installations) {
+            if (installation.equals(sdk.getName())) return sdk;
+        }
+        throw new IllegalArgumentException("Invalid GCloud SDK installation "+installation);
+    }
+
+
+
+    public String getInstallation() {
+        return installation;
+    }
+
+    public String getCredentialsId() {
 		return credentialsId;
 	}
 
@@ -80,5 +114,22 @@ public class GCloudBuildWrapper extends BuildWrapper {
 			save();
 			return super.configure(req, formData);
 		}
+
+        public ListBoxModel doFillCredentialsIdItems(@AncestorInPath Item project,
+                                                     @QueryParameter String serverAddress) {
+            if (project == null || !project.hasPermission(Item.CONFIGURE)) {
+                return new StandardListBoxModel();
+            }
+            return new StandardListBoxModel()
+                    .withEmptySelection()
+                    .withMatching(
+                            MATCHER,
+                            CredentialsProvider.lookupCredentials(GoogleRobotPrivateKeyCredentials.class,
+                                    project,
+                                    ACL.SYSTEM,
+                                    URIRequirementBuilder.fromUri(serverAddress).build()));
+        }
+
+        public static final CredentialsMatcher MATCHER = CredentialsMatchers.anyOf(CredentialsMatchers.instanceOf(GoogleRobotPrivateKeyCredentials.class));
 	}
 }
---
+++ src/main/java/com/cloudbees/jenkins/plugins/gcloudsdk/GCloudInstallation.java
@@ -0,0 +1,64 @@
+package com.cloudbees.jenkins.plugins.gcloudsdk;
+
+import hudson.EnvVars;
+import hudson.Extension;
+import hudson.model.EnvironmentSpecific;
+import hudson.model.Node;
+import hudson.model.TaskListener;
+import hudson.slaves.NodeSpecific;
+import hudson.tools.ToolDescriptor;
+import hudson.tools.ToolInstallation;
+import hudson.tools.ToolProperty;
+import jenkins.model.Jenkins;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
+ */
+public class GCloudInstallation extends ToolInstallation implements NodeSpecific<GCloudInstallation>, EnvironmentSpecific<GCloudInstallation> {
+
+    @DataBoundConstructor
+    public GCloudInstallation(String name, String home, List<? extends ToolProperty<?>> properties) {
+        super(name, home, properties);
+    }
+
+
+    public void buildEnvVars(EnvVars env) {
+        env.override("PATH+GCLOUD", getHome());
+    }
+
+
+    public GCloudInstallation forNode(Node node, TaskListener log) throws IOException, InterruptedException {
+        return new GCloudInstallation(getName(), translateFor(node, log), getProperties().toList());
+    }
+
+    public GCloudInstallation forEnvironment(EnvVars environment) {
+        return new GCloudInstallation(getName(), environment.expand(getHome()), getProperties().toList());
+    }
+
+    public static GCloudInstallation[] getInstallations() {
+        return Jenkins.getInstance().getDescriptorByType(DescriptorImpl.class).getInstallations();
+    }
+
+    @Extension
+    public static class DescriptorImpl extends ToolDescriptor<GCloudInstallation> {
+
+        @Override
+        public String getDisplayName() {
+            return "Google Cloud SDK";
+        }
+
+        public DescriptorImpl() {
+            load();
+        }
+
+        public void setInstallations(GCloudInstallation... installations) {
+            super.setInstallations(installations);
+            save();
+        }
+
+    }
+}
